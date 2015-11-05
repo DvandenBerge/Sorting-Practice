8487d59bdc10a5fdc337fe8edce6382d6c85accb
dvandenberge <dvandenberge@L11703042766.wctc.msft>
2015-11-05 14:40:38 -0600

    added default sort

--- src/sorting/Sorting.java
+++ src/sorting/Sorting.java
@@ -12,9 +12,9 @@ import java.util.TreeSet;
 public class Sorting {
 
     public static void main(String[] args) {
-        Turkey t1=new Turkey("Willy","TKY1",4);
-        Turkey t2=new Turkey("Gobbly","TKY2",6);
-        Turkey t3=new Turkey("Baster","TKY3",5);
+        Turkey t1=new Turkey("Willy",1,4);
+        Turkey t2=new Turkey("Gobbly",2,6);
+        Turkey t3=new Turkey("Baster",3,5);
         
         Set treeSet1=new TreeSet();
         treeSet1.add(t1);
--- src/sorting/Turkey.java
+++ src/sorting/Turkey.java
@@ -1,7 +1,7 @@
 package sorting;
 
 import java.util.Objects;
-import 
+//import org.apache.commons.lang-3.builder
 
 /**
  *
@@ -10,10 +10,10 @@ import
 public class Turkey implements Comparable<Turkey>{
 
     private String name;
-    private String turkeyId;
+    private int turkeyId;
     private int featherCount;
 
-    public Turkey(String name, String turkeyId, int featherCount) {
+    public Turkey(String name, int turkeyId, int featherCount) {
         this.name = name;
         this.turkeyId = turkeyId;
         this.featherCount = featherCount;
@@ -26,10 +26,10 @@ public class Turkey implements Comparable<Turkey>{
         this.name = name;
     }
 
-    public String getTurkeyId() {
+    public int getTurkeyId() {
         return turkeyId;
     }
-    public void setTurkeyId(String turkeyId) {
+    public void setTurkeyId(int turkeyId) {
         this.turkeyId = turkeyId;
     }
 
@@ -75,13 +75,23 @@ public class Turkey implements Comparable<Turkey>{
         return "Turkey{" + "name=" + name + ", turkeyId=" + turkeyId + ", featherCount=" + featherCount + '}';
     }
     
+//    @Override
+//    public int compareTo(Turkey t) {
+//        return new CompareToBuilder().
+//                append(this.name, t.getName()).
+//                append(this.turkeyId, t.getTurkeyId()).
+//                append(this.featherCount, t.getFeatherCount()).
+//                build();
+//    }
+ 
     @Override
-    public int compareTo(Turkey t) {
-        return new CompareToBuilder().
-                append(this.name, t.getName()).
-                append(this.turkeyId, t.getTurkeyId()).
-                append(this.featherCount, t.getFeatherCount()).
-                build();
+    public int compareTo(Turkey t){
+        final int BEFORE=-1;
+        final int EQUAL=0;
+        final int AFTER=1;
+        
+        if(name.equals(t.getName()) && turkeyId==t.getTurkeyId() && featherCount==t.getFeatherCount()) return EQUAL;
+        else if(turkeyId>t.getTurkeyId()) return BEFORE;
+        return AFTER;
     }
-    
 }
